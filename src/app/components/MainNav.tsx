15b216ceb40600939295ee3ffa39c35063265f3e
Tonoy Akanda <tonoy@monetizenow.io>
2023-07-26 18:30:59 +0600

    avatar

--- src/app/components/MainNav.tsx
+++ src/app/components/MainNav.tsx
@@ -1,5 +1,11 @@
 'use client'
-import { Dialog, Disclosure, Popover, Transition } from '@headlessui/react'
+import {
+	Dialog,
+	Disclosure,
+	Menu,
+	Popover,
+	Transition,
+} from '@headlessui/react'
 import {
 	ChevronDownIcon,
 	PhoneIcon,
@@ -153,9 +159,69 @@ export const MainNav = () => {
 					</a>
 				</Popover.Group>
 				<div className="hidden lg:flex lg:flex-1 lg:justify-end">
-					<a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-						Log in <span aria-hidden="true">&rarr;</span>
-					</a>
+					<Menu as="div" className="relative ml-3">
+						<div>
+							<Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+								<span className="sr-only">Open user menu</span>
+								<img
+									className="h-8 w-8 rounded-full"
+									src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+									alt=""
+								/>
+							</Menu.Button>
+						</div>
+						<Transition
+							as={Fragment}
+							enter="transition ease-out duration-100"
+							enterFrom="transform opacity-0 scale-95"
+							enterTo="transform opacity-100 scale-100"
+							leave="transition ease-in duration-75"
+							leaveFrom="transform opacity-100 scale-100"
+							leaveTo="transform opacity-0 scale-95"
+						>
+							<Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+								<Menu.Item>
+									{({ active }) => (
+										<a
+											href="#"
+											className={classNames(
+												active ? 'bg-gray-100' : '',
+												'block px-4 py-2 text-sm text-gray-700'
+											)}
+										>
+											Your Profile
+										</a>
+									)}
+								</Menu.Item>
+								<Menu.Item>
+									{({ active }) => (
+										<a
+											href="#"
+											className={classNames(
+												active ? 'bg-gray-100' : '',
+												'block px-4 py-2 text-sm text-gray-700'
+											)}
+										>
+											Settings
+										</a>
+									)}
+								</Menu.Item>
+								<Menu.Item>
+									{({ active }) => (
+										<a
+											href="#"
+											className={classNames(
+												active ? 'bg-gray-100' : '',
+												'block px-4 py-2 text-sm text-gray-700'
+											)}
+										>
+											Sign out
+										</a>
+									)}
+								</Menu.Item>
+							</Menu.Items>
+						</Transition>
+					</Menu>
 				</div>
 			</nav>
 			<Dialog
